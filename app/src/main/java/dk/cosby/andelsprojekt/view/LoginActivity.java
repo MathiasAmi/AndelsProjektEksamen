3439572761a6edeb1153bb4dcc0e31ba2f0c43c7
MathiasAmi <math35i0@edu.easj.dk>
2019-05-25 15:32:05 +0200

    Added Textwatcher and onclicklistener with firebase

--- app/src/main/java/dk/cosby/andelsprojekt/view/LoginActivity.java
+++ app/src/main/java/dk/cosby/andelsprojekt/view/LoginActivity.java
@@ -1,12 +1,21 @@
 package dk.cosby.andelsprojekt.view;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import dk.cosby.andelsprojekt.R;
 import dk.cosby.andelsprojekt.view.viewmodel.CreateUserViewModel;
@@ -39,6 +48,63 @@ public class LoginActivity extends AppCompatActivity {
         password = (EditText) findViewById(R.id.et_password_login);
         login = (Button) findViewById(R.id.btn_login);
 
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s != null){
+
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        login.setOnClickListener((v)-> mAuth.signInWithEmailAndPassword(email.toString(), password.toString())
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+
+
+                        //updateUI(user);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+
+
+                        //updateUI(null);
+                    }
+
+                    // ...
+                }));
     }
 }
