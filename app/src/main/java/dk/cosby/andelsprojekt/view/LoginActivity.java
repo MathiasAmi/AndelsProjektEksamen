7f4360a8000c4e4a73c10ef39764c626903babfe
MathiasAmi <math35i0@edu.easj.dk>
2019-05-25 14:41:23 +0200

    LoginActivity oprettet med objekter og dens layout

--- app/src/main/java/dk/cosby/andelsprojekt/view/LoginActivity.java
+++ app/src/main/java/dk/cosby/andelsprojekt/view/LoginActivity.java
@@ -1,15 +1,44 @@
 package dk.cosby.andelsprojekt.view;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import dk.cosby.andelsprojekt.R;
+import dk.cosby.andelsprojekt.view.viewmodel.CreateUserViewModel;
+import dk.cosby.andelsprojekt.view.viewmodel.LoginViewModel;
+
+/**
+ * Login view der kontakter LoginViewModel når en EditText-værdi har ændret sig.
+ */
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    private FirebaseAuth mAuth;
+
+    private LoginViewModel viewModel;
+    private EditText email, password;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        viewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        email = (EditText) findViewById(R.id.et_email_login);
+        password = (EditText) findViewById(R.id.et_password_login);
+        login = (Button) findViewById(R.id.btn_login);
+
+
     }
 }
--- app/src/main/java/dk/cosby/andelsprojekt/view/viewmodel/LoginViewModel.java
+++ app/src/main/java/dk/cosby/andelsprojekt/view/viewmodel/LoginViewModel.java
@@ -1,4 +1,17 @@
 package dk.cosby.andelsprojekt.view.viewmodel;
 
-public class LoginViewModel {
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+import dk.cosby.andelsprojekt.model.User;
+
+public class LoginViewModel extends ViewModel {
+
+    private MutableLiveData<String> currentUserEmail = new MutableLiveData<>();
+    private MutableLiveData<String> currentUserPassword = new MutableLiveData<>();
+    private User user = new User();
+
+    public LoginViewModel() {
+
+    }
 }
